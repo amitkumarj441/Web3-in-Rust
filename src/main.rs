23a9c983ab9ac929736289910a4d11a8db3b1a0e
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2017-08-03 17:38:37 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod web3_api;
+
+extern crate rust_web3;
+extern crate hyper;
+extern crate rustc_serialize;
+
+use web3_api::{ Web3Client };
+
+fn main() {
+	let mut web3 = Web3Client::new("http://localhost:8545".to_string());
+	println!("{:?}", web3.get_accounts());
+	println!("{:?}", web3.get_work());
+}
